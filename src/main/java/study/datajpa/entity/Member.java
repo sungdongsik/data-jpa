0f52866a44da4912cc6247af1b7aa69c6671199b
성동식 <tjdehdtlr123@naver.com>
2024-02-19 23:21:15 +0900

    Initial commit

---
+++ src/main/java/study/datajpa/entity/Member.java
@@ -0,0 +1,40 @@
+package study.datajpa.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username","age"}) //연관관계일 때는 toString 하지말기
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+    protected String username;
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    public Member(String username){
+        this.username = username;
+    }
+
+    public void changeTeam(Team team){
+        this.team = team;
+        team.getMembers().add(this);
+    }
+
+    public Member(String username, int age, Team team){
+        this.username = username;
+        this.age = age;
+        if(team != null){
+            changeTeam(team);
+        }
+    }
+}
